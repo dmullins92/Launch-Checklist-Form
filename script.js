57e1c4a6913c5f583e73bd4e9cb68a1c762ef7bb
Dylan Mullins <dylanemullins92@gmail.com>
2020-11-23 11:43:43 -0600

    added form validation to ensure there are values added to each input

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Write your JavaScript code here!
+window.addEventListener('load', function() {
+   let form = document.getElementById('launchForm');
+   form.addEventListener('submit', function(event) {
+      let pilotNameInput = document.querySelector('input[name=pilotName]');
+      let copilotNameInput = document.querySelector('input[name=copilotName]');
+      let fuelLevelInput = document.querySelector('input[name=fuelLevel]');
+      let cargoMassInput = document.querySelector('input[name=cargoMass]');
+      if (pilotNameInput.value === '' 
+      || copilotNameInput.value === ''
+      || fuelLevelInput.value === ''
+      || cargoMassInput.value === '') {
+         alert('All fields are required!');
+         event.preventDefault();
+      }
+      if (typeof pilotNameInput.value === "number"
+      || typeof copilotNameInput.value === "number") {
+         alert('Please enter valid inputs!');
+         event.preventDefault();
+      } 
 
+      
+   });
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
