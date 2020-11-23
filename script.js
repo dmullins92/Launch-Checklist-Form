c97691d8e82aeaa851ecf7db7ceff191408351a9
Dylan Mullins <dylanemullins92@gmail.com>
2020-11-23 15:10:22 -0600

    added fetch and updated form events

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 // Write your JavaScript code here!
-window.addEventListener('load', function() {
+window.addEventListener("load", function() {   
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         let missionTarget = document.getElementById('missionTarget');
+         let randomIndex = Math.floor(Math.random() * json.length);
+         missionTarget.innerHTML  += `<h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[randomIndex].name}</li>
+            <li>Diameter: ${json[randomIndex].diameter}</li>
+            <li>Star: ${json[randomIndex].star}</li>
+            <li>Distance from Earth: ${json[randomIndex].distance}</li>
+            <li>Number of Moons: ${json[randomIndex].moons}</li>
+         </ol>
+         <img src="${json[randomIndex].image}">`
+      });
+   });
+   
    let form = document.getElementById('launchForm');
-   form.addEventListener('submit', function(event) {
+   form.addEventListener("submit", function(event) {
+      //launch status texts
+      let shuttleReadyText = `Shuttle is ready for launch`
+      let shuttleNotReadyText = `Shuttle not ready for launch`;
+      let fuelStatusReadyText = `Fuel level high enough for launch`;
+      let fuelStatusNotReadyText = `Fuel level too low for launch`;
+      let cargoReadyText = `Cargo mass low enough for launch`;
+      let cargoNotReadyText = `Cargo mass too high for launch`;
+      //form inputs
       let pilotNameInput = document.querySelector('input[name=pilotName]');
       let copilotNameInput = document.querySelector('input[name=copilotName]');
       let fuelLevelInput = document.querySelector('input[name=fuelLevel]');
       let cargoMassInput = document.querySelector('input[name=cargoMass]');
+      //document variables
+      let faultyItems = document.getElementById('faultyItems');      
+      let launchStatus = document.getElementById('launchStatus');
+      let pilotStatus = document.getElementById('pilotStatus');
+      let copilotStatus = document.getElementById('copilotStatus');
+      let fuelStatus = document.getElementById('fuelStatus');
+      let cargoStatus = document.getElementById('cargoStatus');
+      //form validation
       if (pilotNameInput.value === '' 
       || copilotNameInput.value === ''
       || fuelLevelInput.value === ''
@@ -15,12 +47,45 @@ window.addEventListener('load', function() {
       }
       let letters = /^[A-Za-z]+$/;
       if (!pilotNameInput.value.match(letters)
-      || !copilotNameInput.value.match(letters)) {
-         alert('Please enter valid inputs for pilot names!');
+      || !copilotNameInput.value.match(letters)
+      || isNaN(Number(fuelLevelInput.value))
+      || isNaN(Number(cargoMassInput.value))) {
+         alert('Please enter valid inputs for all fields!');
+         event.preventDefault();
+      }
+      //document HTML updates
+      if (pilotNameInput.value.match(letters) && copilotNameInput.value.match(letters)) {
+         pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for launch`;
+         pilotStatus.style.visibility = 'visible';
+         copilotStatus.innerHTML = `Co-Pilot ${copilotNameInput.value} is ready for launch`;
+         copilotStatus.style.visibility = 'visible';
+         event.preventDefault();
+      }
+
+      if (fuelLevelInput.value < 10000) {
+         faultyItems.style.visibility = 'visible';
+         launchStatus.innerHTML =  `${shuttleNotReadyText}`;
+         launchStatus.style.color = `red`;
+         fuelStatus.innerHTML = `${fuelStatusNotReadyText}`;
          event.preventDefault();
       } 
-
       
+      if (cargoMassInput.value > 10000) {
+         faultyItems.style.visibility = 'visible';
+         launchStatus.innerHTML =  `${shuttleNotReadyText}`;
+         launchStatus.style.color = 'red';
+         cargoStatus.innerHTML = `${cargoNotReadyText}`;     
+         event.preventDefault(); 
+      }
+
+      if (fuelLevelInput.value >= 10000 && cargoMassInput.value <= 10000) {
+         faultyItems.style.visibility = 'visible';
+         launchStatus.innerHTML = `${shuttleReadyText}`;
+         launchStatus.style.color = 'green';
+         fuelStatus.innerHTML = `${fuelStatusReadyText}`;
+         cargoStatus.innerHTML = `${cargoReadyText}`;
+         event.preventDefault();
+      }    
    });
 });
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
